d82eb3a47776d64edc54fd7977d065b12a8b74de
jonathan <jonathanoliveirasilva9@gmail.com>
2019-10-07 21:08:24 -0300

    ScrollView and RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -26,27 +26,44 @@ class _HomeState extends State<Home> {
       ),
 
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Icon(Icons.person_pin, size: 120.0, color: Colors.greenAccent),
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: 'Weight(kg)',
-              labelStyle: TextStyle(color: Colors.greenAccent)
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Icon(Icons.person_pin, size: 120.0, color: Colors.greenAccent),
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: 'Weight(kg)',
+                  labelStyle: TextStyle(color: Colors.greenAccent)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),
+            TextField(keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  labelText: 'Heigth(cm)',
+                  labelStyle: TextStyle(color: Colors.greenAccent)
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),
+            ),
+          Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+            child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Calculator', style: TextStyle(color: Colors.white, fontSize: 25.0),),
+                  color: Colors.greenAccent,
+                ),
+              )
           ),
-          TextField(keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-                labelText: 'Heigth(cm)',
-                labelStyle: TextStyle(color: Colors.greenAccent)
-            ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),
-          )
-        ],
+            Text('Info',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.greenAccent, fontSize: 25.0),)
+          ],
+        ),
       ),
     );
   }
