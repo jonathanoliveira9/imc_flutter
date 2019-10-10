f2497843b26e955e7bae3bc1bd11a0e9ddca1044
jonathan <jonathanoliveirasilva9@gmail.com>
2019-10-09 21:36:11 -0300

    Fix reload message validation

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _HomeState extends State<Home> {
     heightController.text = '';
     setState(() {
       _infoText = 'Insert your data!';
+      _formKey = GlobalKey<FormState>();
     });
   }
 
